2d7fdf0320ea838a834cb72c5d80e90f1a1550cc
weinenwollen <tsvetkov.ti@edu.spbstu.ru>
2023-03-26 02:03:31 +0300

    [codedrop] Init files

---
+++ process-enjoyer/main.cpp
@@ -0,0 +1,11 @@
+#include "processenjoyer.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[]){
+    // Egor loh
+
+    QApplication a(argc, argv);
+    processenjoyer w;
+    w.show();
+    return a.exec();
+}
---
+++ process-enjoyer/processenjoyer.cpp
@@ -0,0 +1,10 @@
+#include "processenjoyer.h"
+
+processenjoyer::processenjoyer(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+processenjoyer::~processenjoyer()
+{}

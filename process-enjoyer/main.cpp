08271be0b48672607c4efd88d402a4d968dbe893
Inimities <lavrenko.es@edu.spbstu.ru>
2023-03-27 23:11:58 +0300

    [codedrop] Добавлены файлы, необходимые для инициализации структур процессов (будут расширены)

--- process-enjoyer/main.cpp
+++ process-enjoyer/main.cpp
@@ -1,6 +1,8 @@
 #include "processenjoyer.h"
 #include <QtWidgets/QApplication>
 
+#include "backend.h" // added
+
 int main(int argc, char *argv[]){
     // Egor loh
 
@@ -8,4 +10,19 @@ int main(int argc, char *argv[]){
     processenjoyer w;
     w.show();
     return a.exec();
+
+    // part that need to fill the processes struct //
+
+    setlocale(LC_ALL, "");
+
+    DWORD sePrivilege = SE_PRIVILEGE_ENABLED;
+    turnDebugPrivilege(sePrivilege);
+
+    processesDatabase();
+
+    sePrivilege = 0;
+    turnDebugPrivilege(sePrivilege);
+
+    system("pause");
+    return 0;
 }

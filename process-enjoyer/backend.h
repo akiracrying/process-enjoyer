1f544e8013a229b8a97caea660cdb5a2a5d86ba9
launew <103151764+launew@users.noreply.github.com>
2023-03-30 02:53:27 +0300

    [update] Структура процессов, mandatory и pipe

--- process-enjoyer/backend.h
+++ process-enjoyer/backend.h
@@ -3,20 +3,25 @@
 #include <windows.h>
 #include <stdio.h>
 #include <tchar.h>
+
+#pragma comment(lib,"Version.lib")
+
 #include <psapi.h>
 #include <sddl.h>
 #include <tlhelp32.h>
+#include <winver.h>
 
-#define MAX_PROC_COUNT 1024
+#include <AclAPI.h>
 
-#define MAX_NAME_LENGTH 256
-#define MAX_PATH_LENGTH 512
-#define MAX_DETAILS		32
-#define MAX_TYPE_LENGTH 7
+#define MAX_COUNT 1024
+
+#define MAX_NAME_LENGTH		256
+#define MAX_DETAILS_LENGTH	32
+#define MAX_TYPE_LENGTH		7
 
 /* YES, these are not getters - just load info about process to struct process */
 
-size_t getOwnerAndSid(HANDLE);
+void getOwnerAndSid(HANDLE);
 void getDepAndAslr(HANDLE);
 void getParentPidAndName(DWORD);
 void getIntegrityLevel(HANDLE);
@@ -29,23 +34,30 @@ void processInfo(DWORD);
 void processesDatabase();
 
 BOOL turnDebugPrivilege(DWORD);
+void changeProcIntegrity(DWORD, wchar_t*);
+
+void getFileIntegrity(const wchar_t*);
+void changeFileIntegrity();
 
 typedef struct process
 {
 	DWORD PID;
 	wchar_t processName[MAX_NAME_LENGTH];
-	wchar_t pathProcessExe[MAX_PATH_LENGTH];
+	wchar_t pathProcessExe[MAX_PATH];
 	wchar_t processOwner[MAX_NAME_LENGTH];
 	wchar_t SID[MAX_NAME_LENGTH];
 	wchar_t procType[MAX_TYPE_LENGTH];
-	DWORD integrityLevel;
+	wchar_t integrityLevel[MAX_DETAILS_LENGTH];
+	wchar_t* procDescryption;
 
+	BOOL CLR;
 	BOOL ASLR;
 	BOOL DEP;
-	wchar_t aslrDetails[MAX_DETAILS];
-	wchar_t depDetails[MAX_DETAILS];
+	wchar_t aslrDetails[MAX_DETAILS_LENGTH];
+	wchar_t depDetails[MAX_DETAILS_LENGTH];
 
 	wchar_t parentName[MAX_NAME_LENGTH];
 	DWORD parentPID;
 
+	wchar_t processDllsName[MAX_COUNT][MAX_NAME_LENGTH];
 }process;

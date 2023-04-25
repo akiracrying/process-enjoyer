b02b98a02f7cae973d103909f21797940798d224
Тимофей Цветков <tsvetkov.ti@edu.spbstu.ru>
2023-04-25 10:19:09 +0300

    [codedrop] Сделал визуальный прикол

--- process-enjoyer/main.cpp
+++ process-enjoyer/main.cpp
@@ -1,11 +1,17 @@
 #include "processenjoyer.h"
 #include <QtWidgets/QApplication>
-
+#include <QtCore5Compat/QTextCodec>
 #include "backend.h" // added
+
 
 int main(int argc, char *argv[]){
     // Egor loh
     QApplication a(argc, argv);
+
+    setlocale(LC_ALL, "Russian");
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+  /*  QTextCodec* russian = QTextCodec::codecForName("CP1251");
+    QTextCodec::setCodecForLocale(russian);*/
     processenjoyer w;
     w.show();
     return a.exec();

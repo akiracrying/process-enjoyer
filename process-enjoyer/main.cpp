72acf77fffcf568c0c87764a471649bc2bd3b425
Тимофей Цветков <tsvetkov.ti@edu.spbstu.ru>
2023-05-03 02:55:04 +0300

    [feature] Popup Dll window

--- process-enjoyer/main.cpp
+++ process-enjoyer/main.cpp
@@ -1,17 +1,12 @@
 #include "processenjoyer.h"
 #include <QtWidgets/QApplication>
-#include <QtCore5Compat/QTextCodec>
+#include <QStringConverter>
 #include "backend.h" // added
-
 
 int main(int argc, char *argv[]){
     // Egor loh
     QApplication a(argc, argv);
 
-    setlocale(LC_ALL, "Russian");
-   // QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
-  /*  QTextCodec* russian = QTextCodec::codecForName("CP1251");
-    QTextCodec::setCodecForLocale(russian);*/
     processenjoyer w;
     w.show();
     return a.exec();
